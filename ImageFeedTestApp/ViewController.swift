13b2fed78efc38bfb90d88f48a04b7b644b0b28b
Sbat2000 <zevzok@gmail.com>
2024-09-08 22:42:05 +0800

    Initial Commit

---
+++ ImageFeedTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeedTestApp
+//
+//  Created by Aleksandr Garipov on 08.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
