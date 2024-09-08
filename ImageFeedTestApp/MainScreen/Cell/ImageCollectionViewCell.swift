2fc64cf1880e65a954f7b78a66c9bf7019feea68
Sbat2000 <zevzok@gmail.com>
2024-09-08 23:37:22 +0800

    [MS] добавил ячейку для коллекции фото

---
+++ ImageFeedTestApp/MainScreen/Cell/ImageCollectionViewCell.swift
@@ -0,0 +1,66 @@
+//
+//  ImageCollectionViewCell.swift
+//  ImageFeedTestApp
+//
+//  Created by Aleksandr Garipov on 08.09.2024.
+//
+
+import UIKit
+
+final class ImageCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - UI Elements
+
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .darkGray
+        label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Private Methods
+
+private extension ImageCollectionViewCell {
+
+    func setupUI() {
+        contentView.addSubview(imageView)
+        contentView.addSubview(descriptionLabel)
+    }
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.7),
+
+            descriptionLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            descriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            descriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        ])
+    }
+}
